2e65d38bd470efd692dbad9bf63b224bb5c04ba3
MatheusAkira <matheusfelipeakira@gmail.com>
2023-12-08 21:57:04 -0300

    upload

--- simulador_cache.py
+++ simulador_cache.py
@@ -22,21 +22,25 @@ class CacheSimulator:
         self.miss = 0
         self.counter_cache = 0
         self.V = [0] * self.n_lines
+        self.aux = [0] * self.size_groups
        
     
     def acessar_dado(self, idx, dado):
             if dado in self.cache[idx]:
                 self.hits +=1
+
+                
             else:
+
                 self.miss +=1
                 if len(self.cache[idx]) < self.line_per_group:
+                    self.V[(idx * self.line_per_group) +(len(self.cache[idx])) ] = 1
                     self.cache[idx].append(dado)
-                    self.V[(idx * self.line_per_group) + len(self.cache[idx]) ] = 1
-                    
                     
                 else:
-                    new_pos = len(self.cache[idx]) % self.line_per_group
+                    new_pos = (len(self.cache[idx]) + self.aux[idx]) % self.line_per_group
                     self.cache[idx][new_pos] = dado
+                    self.aux[idx] +=1
                     
 
     #printar cachê
@@ -49,7 +53,7 @@ class CacheSimulator:
         for linha in range (self.size_groups):
             for coluna in range(self.line_per_group):
                 if 0 <= linha < len(self.cache) and 0 <= coluna < len(self.cache[linha]):
-                    print(f"00{idx_print} {self.V[coluna]}  {self.cache[linha][coluna]}")
+                    print(f"00{idx_print} {self.V[coluna]} {self.cache[linha][coluna]}")
                     idx_print += 1
                 else:
                     print(f"00{idx_print} {self.V[coluna]}") 
@@ -106,11 +110,10 @@ class CacheSimulator:
                 
                 self.acessar_dado(idx_block, hexa_value)
                 self.print_cache()
-
-                                
+                    
         print('\n')
-        print("hits: ", self.hits)
-        print("miss: ", self.miss)
+        print("#hits: ", self.hits)
+        print("#miss: ", self.miss)
                     
 
 
